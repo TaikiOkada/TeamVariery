a2849a3b6fb7b669e655f2a2b055aaa5eff44c5b
Ryota <ryota_arai@cysista.co.jp>
2021-06-11 14:45:15 +0900

    6/11 荒井　会員登録画面レイアウト変更(未完)

--- shared_shop/src/main/java/jp/co/sss/shop/controller/user/UserRegistCustomerController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/user/UserRegistCustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.sss.shop.form.UserForm;
 import jp.co.sss.shop.repository.UserRepository;
 
 /**
@@ -43,6 +44,17 @@ public class UserRegistCustomerController {
 		return "user/regist/user_regist_input";
 	}
 
+	/**
+	 * POSTメソッドを利用して会員情報入力画面に戻る処理
+	 *
+	 * @param form 会員情報
+	 * @return "user/regist/user_regist_input" 会員情報 登録入力画面へ
+	 */
+	@RequestMapping(path = "/user/regist/input", method = RequestMethod.POST)
+	public String registInput(UserForm form) {
+
+		return "user/regist/user_regist_input";
+	}
 
 	/**
 	 * 会員情報確認画面表示処理
@@ -57,7 +69,7 @@ public class UserRegistCustomerController {
 	}
 
 	/**
-	 * 会員情報完了画面表示
+	 * 会員情報完了画面表示処理
 	 *
 	 * @param model
 	 * @return

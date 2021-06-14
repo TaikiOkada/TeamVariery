dbbb7440b3574330b25f44b98efb955088ad387d
sekit1998 <sekit@ark-system.co.jp>
2021-06-14 11:06:21 +0900

    0614 関　確認用（生田目）

---
+++ shared_shop/src/main/java/jp/co/sss/shop/repository/FeeRepository.java
@@ -0,0 +1,10 @@
+package jp.co.sss.shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.co.sss.shop.entity.Fee;
+
+public interface FeeRepository extends JpaRepository<Fee, Integer>{
+
+
+}

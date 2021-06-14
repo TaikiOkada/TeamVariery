4e9d5b314ce7c19f7352fcf20607ed70488e8d31
sekit1998 <sekit@ark-system.co.jp>
2021-06-14 11:00:20 +0900

    0614 関　送料詳細画面、価格表示完了

--- shared_shop/src/main/java/jp/co/sss/shop/controller/order/DeliveryFeeController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/order/DeliveryFeeController.java
@@ -1,18 +1,23 @@
 package jp.co.sss.shop.controller.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.sss.shop.repository.FeeRepository;
 
 @Controller
 
 public class DeliveryFeeController {
+	@Autowired
+	FeeRepository feeRepository;
 
 	@RequestMapping(path="/confirm" ,method =RequestMethod.GET )
-	public String showDeliveryFee() {
-		System.out.println("あああ");
-
-		return "confirm/Confirm";
+	public String showDeliveryFee(Model model) {
+			model.addAttribute("delivery_fee",feeRepository.findAll());
+			return "confirm/Confirm";
 	}
 
 }

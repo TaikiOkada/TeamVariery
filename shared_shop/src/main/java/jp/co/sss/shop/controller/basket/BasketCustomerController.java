ad19904ebd484a01d38d66211679839e0a5fd306
Koido <k-koido@m-brain.jp>
2021-06-14 11:45:22 +0900

    0614 古井戸　買い物かご画面遷移

--- shared_shop/src/main/java/jp/co/sss/shop/controller/basket/BasketCustomerController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/basket/BasketCustomerController.java
@@ -1,5 +1,6 @@
 package jp.co.sss.shop.controller.basket;
 
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,9 +8,59 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class BasketCustomerController {
 
+	/**
+	 * 買い物かごに追加
+	 *
+	 * @return basket/shopping_basket 買い物かご画面
+	 */
+	@RequestMapping(path = "/basket/add", method = RequestMethod.POST)
+	public String addItem(Session session) {
+		System.out.println("買い物add");
+		return basketList();
+	}
+
+	/**
+	 * 対象商品を買い物かごから削除
+	 *
+	 * @return basket/shopping_basket 買い物かご画面
+	 */
+	@RequestMapping(path = "/basket/delete", method = RequestMethod.POST)
+	public String deleteItem(Session session) {
+		System.out.println("買い物delete");
+		return basketList();
+	}
+
+	/**
+	 * 全商品を買い物かごから削除
+	 *
+	 * @return basket/shopping_basket 買い物かご画面
+	 */
+	@RequestMapping(path = "/basket/allDelete", method = RequestMethod.POST)
+	public String allDelete(Session session) {
+		System.out.println("買い物allDelete");
+		return basketList();
+	}
+
+	/**
+	 * ナビゲーションバーから買い物かご画面表示
+	 *
+	 * @return basket/shopping_basket 買い物かご画面
+	 */
 	@RequestMapping(path = "/basket/list", method = RequestMethod.GET)
-	public String showBasket() {
+	public String basketListGet() {
 
 		return "basket/shopping_basket";
 	}
+
+	/**
+	 * 買い物かご画面表示
+	 *
+	 * @return basket/shopping_basket 買い物かご画面
+	 */
+	@RequestMapping(path = "/basket/list", method = RequestMethod.POST)
+	public String basketList() {
+
+		return "basket/shopping_basket";
+	}
+
 }
--- shared_shop/src/main/java/jp/co/sss/shop/controller/order/OrderShowCustomerController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/order/OrderShowCustomerController.java
@@ -1,5 +1,8 @@
 package jp.co.sss.shop.controller.order;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class OrderShowCustomerController {
 
 }

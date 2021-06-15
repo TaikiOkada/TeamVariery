4d70aec3368966b76791ebe5c6efc546c37dc199
TaikiOkada <yaritaikoto98@gmail.com>
2021-06-15 13:40:21 +0900

    新着順売れ筋順でない

--- shared_shop/src/main/java/jp/co/sss/shop/repository/OrderItemRepository.java
+++ shared_shop/src/main/java/jp/co/sss/shop/repository/OrderItemRepository.java
@@ -1,8 +1,12 @@
 package jp.co.sss.shop.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import jp.co.sss.shop.entity.Category;
+import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.entity.OrderItem;
 
 /**
@@ -11,5 +15,5 @@ import jp.co.sss.shop.entity.OrderItem;
  * @author System Shared
  */
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
-
+	//<Quantity> List<OrderItem> findByQuantity(Quantity quantity);
 }

a2824217bb21bd588a34263b7a61cdc7e90e0a59
Koido <k-koido@m-brain.jp>
2021-06-17 11:33:37 +0900

    0617 古井戸　買い物かごの中身を注文確認画面へ反映

--- shared_shop/src/main/java/jp/co/sss/shop/controller/order/OrderRegistCustomerController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/order/OrderRegistCustomerController.java
@@ -1,21 +1,31 @@
 package jp.co.sss.shop.controller.order;
 
-import org.springframework.beans.BeanUtils;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.sss.shop.bean.BasketBean;
+import jp.co.sss.shop.bean.ItemBean;
 import jp.co.sss.shop.bean.OrderItemBean;
-import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.form.OrderShowForm;
+import jp.co.sss.shop.repository.ItemRepository;
 import jp.co.sss.shop.repository.OrderRepository;
+import jp.co.sss.shop.util.BeanCopy;
 
 @Controller
 public class OrderRegistCustomerController {
 	@Autowired
 	OrderRepository orderRepository;
+	@Autowired
+	ItemRepository itemRepository;
 	/**
 	 * 届け先入力画面表示
 	 *
@@ -44,15 +54,53 @@ public class OrderRegistCustomerController {
 	* @return order/regist/order_check 画面
 	*/
 	@RequestMapping(path = "/order/check", method = RequestMethod.POST)
-	public String checkOrder(@ModelAttribute OrderShowForm form ,Integer stock) {
+	public String checkOrder(@ModelAttribute OrderShowForm form ,Integer stock, Model model, HttpSession session) {
+		// 合計値段
+		int totalNum = 0;
+		int subTotalNum = 0;
+
+		// 表示用リスト
+		List<OrderItemBean> orderItemBeanList = new ArrayList<OrderItemBean>();
 		OrderItemBean orderItemBean = new OrderItemBean();
 
-		//登録情報の生成
-		Item items = new Item();
+		// 買い物かごの中身をリストに代入
+		@SuppressWarnings("unchecked")
+		List<BasketBean> basketBeanList = ((List<BasketBean>) session.getAttribute("baskets"));
+
+		// 商品情報の生成
+		ItemBean itemBean = new ItemBean();
 		//入力値を登録情報にコピーする
-		BeanUtils.copyProperties(form, items);
-		//登録情報を保存
-		//orderRepository.save(items);
+//		BeanUtils.copyProperties(form, items);
+
+		// 買い物かごリストの中身を注文リストにコピーする
+		for (BasketBean basketBean : basketBeanList) {
+			itemBean = BeanCopy.copyEntityToBean(itemRepository.getOne(basketBean.getId()));
+			orderItemBean = new OrderItemBean();
+
+			orderItemBean.setId(basketBean.getId());				// ID
+			orderItemBean.setName(basketBean.getName());			// 名前
+			orderItemBean.setOrderNum(basketBean.getOrderNum());	// 注文数
+			orderItemBean.setPrice(itemBean.getPrice());			// 値段
+			orderItemBean.setImage(itemBean.getImage());			// 画像
+			orderItemBean.setSubtotal(itemBean.getPrice() * basketBean.getOrderNum());	// 小計
+
+			// リストに追加 ここに在庫数チェック？
+			orderItemBeanList.add(orderItemBean);
+		}
+
+		// 注文リストの商品値段の合計
+		for (OrderItemBean bean : orderItemBeanList) {
+			subTotalNum += bean.getSubtotal();
+		}
+		totalNum = subTotalNum;
+
+		// 注文リストをリクエストスコープに追加
+		model.addAttribute("orderItems", orderItemBeanList);
+
+		// 合計値段をリクエストスコープに追加
+		model.addAttribute("subTotalNum", subTotalNum);		// 商品合計
+		model.addAttribute("totalNum", totalNum);			// 合計
+
 		return "order/regist/order_check";
 	}
 
@@ -63,6 +111,8 @@ public class OrderRegistCustomerController {
 	*/
 	@RequestMapping(path = "/order/complete", method = RequestMethod.POST)
 	public String completeOrder() {
+		//登録情報を保存
+		//orderRepository.save(items);
 
 		return "order/regist/order_complete";
 	}

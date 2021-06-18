96be68634326b91ae3b60cc658d912ae0c25ebc3
Koido <k-koido@m-brain.jp>
2021-06-18 17:47:37 +0900

    0618 古井戸　注文登録処理仮終了

--- shared_shop/src/main/java/jp/co/sss/shop/controller/order/OrderRegistCustomerController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/order/OrderRegistCustomerController.java
@@ -20,7 +20,9 @@ import jp.co.sss.shop.bean.ItemBean;
 import jp.co.sss.shop.bean.OrderBean;
 import jp.co.sss.shop.bean.OrderItemBean;
 import jp.co.sss.shop.bean.UserBean;
+import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.entity.Order;
+import jp.co.sss.shop.entity.OrderItem;
 import jp.co.sss.shop.entity.User;
 import jp.co.sss.shop.form.OrderForm;
 import jp.co.sss.shop.repository.ItemRepository;
@@ -184,34 +186,41 @@ public class OrderRegistCustomerController {
 
 		orderRepository.save(order);
 
-//		OrderItem orderItem = new OrderItem();
-//		ItemBean itemBean = new ItemBean();
-//
-//		// 買い物かごの中身をリストに代入
-//		@SuppressWarnings("unchecked")
-//		List<BasketBean> basketBeanList = ((List<BasketBean>) session.getAttribute("baskets"));
-//
-//		List<OrderItemBean> orderItemBeanList = new ArrayList<OrderItemBean>();
-//		BeanUtils.copyProperties(orderItemBean, orderItem);
-//		orderItem.setQuantity(orderItemBean.getOrderNum());
-//		for (BasketBean basketBean : basketBeanList) {
-//			itemBean = BeanCopy.copyEntityToBean(itemRepository.getOne(basketBean.getId()));
-//			orderItemBean = new OrderItemBean();
-//
-//			orderItemBean.setId(basketBean.getId());				// ID
-//			orderItemBean.setName(basketBean.getName());			// 名前
-//			orderItemBean.setOrderNum(basketBean.getOrderNum());	// 注文数
-//			orderItemBean.setPrice(itemBean.getPrice());			// 値段
-//			orderItemBean.setImage(itemBean.getImage());			// 画像
-//			orderItemBean.setSubtotal(itemBean.getPrice() * basketBean.getOrderNum());	// 小計
-//
-//			// リストに追加 ここに在庫数チェック？
-//			orderItemBeanList.add(orderItemBean);
-//		}
-//
-//		System.out.println(orderItem.getQuantity());
-//
-//		orderItemRepository.save(orderItem);
+		// 注文商品テーブルへ情報を保存
+		OrderItem orderItem = new OrderItem();
+		ItemBean itemBean = new ItemBean();
+		Item item = new Item();
+
+		// 買い物かごの中身をリストに代入
+		@SuppressWarnings("unchecked")
+		List<BasketBean> basketBeanList = ((List<BasketBean>) session.getAttribute("baskets"));
+
+		List<OrderItemBean> orderItemBeanList = new ArrayList<OrderItemBean>();
+		BeanUtils.copyProperties(orderItemBean, orderItem);
+		// 買い物かごの中身数分 回す
+		for (BasketBean basketBean : basketBeanList) {
+			item = new Item();
+			itemBean = BeanCopy.copyEntityToBean(itemRepository.getOne(basketBean.getId()));
+			BeanUtils.copyProperties(itemBean, item);
+			orderItemBean = new OrderItemBean();
+
+			orderItemBean.setId(basketBean.getId());				// ID
+			orderItemBean.setName(basketBean.getName());			// 名前
+			orderItemBean.setOrderNum(basketBean.getOrderNum());	// 注文数
+			orderItemBean.setPrice(itemBean.getPrice());			// 値段
+			orderItemBean.setImage(itemBean.getImage());			// 画像
+			orderItemBean.setSubtotal(itemBean.getPrice() * basketBean.getOrderNum());	// 小計
+
+			// リストに追加 ここに在庫数チェック？
+			orderItemBeanList.add(orderItemBean);
+
+			orderItem = new OrderItem();
+			orderItem.setQuantity(orderItemBean.getOrderNum());
+			orderItem.setOrder(order);
+			orderItem.setPrice(orderItemBean.getPrice());
+			orderItem.setItem(item);
+			orderItemRepository.save(orderItem);
+		}
 
 		return "order/regist/order_complete";
 	}
